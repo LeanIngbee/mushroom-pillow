4901db11cfb00d7c6a9dfd29be8dc06a40dd04a8
janselmi <janselmi@auction.com>
2022-04-11 12:19:13 -0300

    changing altafonte logic for new file

--- scripts/clean/altafonte.sql
+++ scripts/clean/altafonte.sql
@@ -3,20 +3,20 @@ create or replace view clean.altafonte as
 
 SELECT
      "$path" file,
-    TRY_CAST("concat"("fecha reporte", '-01') AS date) report_date,
+    TRY_CAST("concat"("fecha liquidacion", '-01') AS date) report_date,
     TRY_CAST("concat"("fecha_consumo", '-01') AS date) sale_date,
     TRY_CAST("suma de cantidad" AS bigint) quantity,
     'VENTA DIGITAL' sale_type,
-    NULL gross_revenue, --ASK TOMAS IF NULL OR = NET_REVENUE
+    TRY_CAST(REPLACE(REPLACE("suma de bruto altafonte", '.', ''), ',','.') as double ) gross_revenue,
     TRY_CAST(REPLACE(REPLACE("suma de neto altafonte", '.', ''), ',','.') as double ) net_revenue,
-    TRIM("referencia") product_id,
+    TRIM("mp") product_id,
     TRIM("isrc") isrc,
-    NULL platform,
+    NULLIF("lower"(TRIM("plataforma")), '') platform,
     'ALTAFONTE' source,
     NULLIF("lower"(country), '') country,
     CAST(null AS varchar) agency,
     false is_licencing,
-    NULLIF("lower"("tipo de ingreso"), '') operation_type,
+    NULLIF("lower"("tipo de operación"), '') operation_type,
     NULL stream_quality
 
 FROM raw.altafonte
