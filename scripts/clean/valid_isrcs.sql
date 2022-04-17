f71d5e9b9961e87042ce79481d9aa65891ea85c8
Leandro Palma <lpalma@fi.uba.ar>
2022-04-17 19:11:45 -0300

    Only consider audio database as source of truth for valid product ids and isrcs

--- scripts/clean/valid_isrcs.sql
+++ scripts/clean/valid_isrcs.sql
@@ -1,4 +1,6 @@
 --create database if not exists clean
 create or replace view clean.valid_isrcs as 
 
-select distinct isrc from clean.valid_isrc_product_id_pair
+select distinct isrc 
+from raw.audio
+where isrc is not null
