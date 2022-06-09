c3e4c0f5ce0dd6ae65c313ba0904fa18e8c628ad
Leandro Palma <lpalma@fi.uba.ar>
2022-06-09 08:09:27 -0300

    Remove duplicates from exchange_rates table.

--- scripts/clean/exchange_rates.sql
+++ scripts/clean/exchange_rates.sql
@@ -1,6 +1,10 @@
 --create database if not exists clean
 create or replace view clean.exchange_rates as 
 select
- date_parse(date, '%Y-%m-%d') as "date",
- base, target, cast(rate as double) as rate
+    date_parse(date, '%Y-%m-%d') as "date",
+    base, 
+    target, 
+    min_by(cast(rate as double), date) as rate
+
 from raw.exchange_rates
+group by 1, 2, 3
